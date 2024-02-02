94e0ba5b6a6be18a54d3fa50b140faa38b633bca
Demetryj <103371743+Demetryj@users.noreply.github.com>
2024-02-02 22:22:51 +0200

    Update toggle-mobile-menu.js

--- js/toggle-mobile-menu.js
+++ js/toggle-mobile-menu.js
@@ -1,6 +1,9 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const navLinkRef1 = document.querySelector('[ data-link-1]');
+  const navLinkRef2 = document.querySelector('[ data-link-2]');
+  const navLinkRef3 = document.querySelector('[ data-link-3]');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
@@ -21,6 +24,36 @@
       menuBtnRef.setAttribute('aria-expanded', !expanded);
 
       mobileMenuRef.classList.remove('is-open');
+    }
+  });
+
+  navLinkRef1.addEventListener('click', () => {
+    if (mobileMenuRef.classList.contains('is-open')) {
+      menuBtnRef.classList.remove('is-open');
+
+      mobileMenuRef.classList.remove('is-open');
+    } else {
+      return;
+    }
+  });
+
+  navLinkRef2.addEventListener('click', () => {
+    if (mobileMenuRef.classList.contains('is-open')) {
+      menuBtnRef.classList.remove('is-open');
+
+      mobileMenuRef.classList.remove('is-open');
+    } else {
+      return;
+    }
+  });
+
+  navLinkRef3.addEventListener('click', () => {
+    if (mobileMenuRef.classList.contains('is-open')) {
+      menuBtnRef.classList.remove('is-open');
+
+      mobileMenuRef.classList.remove('is-open');
+    } else {
+      return;
     }
   });
 })();
